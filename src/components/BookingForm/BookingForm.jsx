33a483b0040bb869edc91755999c7c7e5b50f07f
Arjun7R <arjunkumar58608@gmail.com>
2024-07-15 09:39:12 +0530

    move to assets folder

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,11 +1,12 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import horses from "../HorsesList";
 import { RxCross2 } from "react-icons/rx";
 import axios from "axios";
 import "./style.css";
 import { Link, useNavigate } from "react-router-dom";
-import {toast, ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const url = "https://65fd057c9fc4425c6530ff71.mockapi.io/booking";
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -15,25 +16,53 @@ const nameRegex = /^[a-zA-Z][a-zA-Z\s'-]{0,49}$/;
 function BookingForm({}) {
   const [date, setDate] = useState("");
   const [getData, setGetData] = useState({});
+  const [time, setTime] = useState("");
+  const [currentDay, setCurrentDay] = useState("");
   const [formData, setFormData] = useState({
-    selectedHorse: "",
-    date: "",
-    name: "",
-    email: "",
-    phone: "",
+    selectedHorse: {
+      value: "",
+      errorMessage: "",
+    },
+    date: {
+      value: "",
+      errorMessage: "",
+    },
+    name: {
+      value: "",
+      errorMessage: "",
+    },
+    email: {
+      value: "",
+      errorMessage: "",
+    },
+    phone: {
+      value: "",
+      errorMessage: "",
+    },
   });
 
   const navigate = useNavigate();
+
+  const getCurrentDate = () => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    return `${yyyy}-${mm}-${dd}`;
+  };
+
+  const [minDate, setMinDate] = useState(getCurrentDate());
 
   const hanldeDateInput = (userDate) => {
     setDate(userDate);
   };
 
   const handleChange = (key, value) => {
-    setFormData({ ...formData, [key]: value });
+    setFormData({ ...formData, [key]: { value, errorMessage: "" } });
   };
-
+  
   const handleTimeInput = (bookingTime) => {
+    setTime(bookingTime);
     const [hours, minutes] = bookingTime.split(":");
     const newDate = new Date(date).setHours(hours, minutes);
     const finalDate = new Date(newDate).toLocaleString();
@@ -43,41 +72,97 @@ function BookingForm({}) {
   const postBookingData = async () => {
     try {
       const result = await axios.post(url, { ...formData });
-      console.log(result.data);
+      if(result !== "") {
+        return true;
+      } else {
+        return false;
+      }
     } catch (error) {
       console.log(`error while posting data`);
     }
   };
 
-  const isButtonEnable = Object.values(formData).every((value) => value !== "");
+  // const isButtonEnable = Object.values(formData).every(item => item.value !== ""); // here we continue the work
 
-  const getDetails = async() => {
+  const getDetails = async () => {
     try {
-      const response = (await axios.get("https://65fd057c9fc4425c6530ff71.mockapi.io/booking")).data;
+      const response = (
+        await axios.get("https://65fd057c9fc4425c6530ff71.mockapi.io/booking")
+      ).data;
       setGetData(response[response.length - 1]);
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getDetails();
-  }, [])
+  }, []);
 
+  const onNameBlur = () => {
+    if(!nameRegex.test(formData?.name?.value)) {
+      setFormData({
+        ...formData,
+        name: {
+          value: formData?.name?.value,
+          errorMessage: "please enter a valid name",
+        }
+      });
+    }
+  }
+
+  const onEmailBlur = () => {
+    if(!emailRegex.test(formData.email.value)) {
+      setFormData({
+        ...formData,
+        email: {
+          value :formData?.email?.value,
+          errorMessage: "please enter a valid email",
+        }
+      });
+    }
+  }
+
+  const onPhoneBlur = () => {
+    if(!phoneRegex.test(formData?.phone?.value)) {
+      setFormData({
+        ...formData,
+        phone: {
+          value :formData?.phone?.value,
+          errorMessage: "please enter a valid phone",
+        }
+      });
+    }
+  }
+
+  const onSelectionBlur = () => {
+    if(!formData?.selectedHorse?.value) {
+      setFormData({
+        ...formData,
+        selectedHorse: {
+          value :formData?.selectedHorse?.value,
+          errorMessage: "please select a Horse",
+        }
+      });
+    }
+  }
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (isButtonEnable) {
+    if (true) { 
       postBookingData();
-      toast.success(<div className="invite">
-        <div className="invite-div">
-          <label htmlFor="">Horse Name : </label>
-          <label style={{color:"#283618"}} htmlFor="">{getData.selectedHorse}</label>
-        </div>
-        <p>Thanks For Booking 🙂</p>
-      </div>);
       navigate("/");
-    } else {
-      toast.error("Form Not Submitted !");
+      toast.success(
+        <div className="invite">
+          <div className="invite-div">
+            <label htmlFor="">Horse Name : </label>
+            <label style={{ color: "#283618" }} htmlFor="">
+              {getData.selectedHorse.value}
+            </label>
+          </div>
+          <p>Thanks For Booking 🙂</p>
+        </div>
+      );
     }
   };
 
@@ -93,11 +178,14 @@ function BookingForm({}) {
         <div>
           <select
             onClick={(e) => handleChange("selectedHorse", e.target.value)}
-          >
+            className={formData?.selectedHorse?.errorMessage ? "error-field" : ""}
+            onBlur={(e) => onSelectionBlur()}
+            >
             {horses?.map((horse) => (
               <option key={horse.id}>{horse.name}</option>
             ))}
           </select>
+            {formData?.selectedHorse?.errorMessage && <p className="error">this is the message</p>}
         </div>
         <div className="date-time-container">
           <div className="Date-label-div">
@@ -106,15 +194,19 @@ function BookingForm({}) {
           </div>
           <div className="date-container">
             <input
-              onChange={(e) => hanldeDateInput(e.target.value)}
+              onChange={(e) => {hanldeDateInput(e.target.value)}}
               type="Date"
+              value={date}
               className=""
-            />
+              min={minDate}
+              />
             <input
+              value={time}
               onChange={(e) => handleTimeInput(e.target.value)}
               type="time"
-            />
+              />
           </div>
+            {date && time == "" ? <p className="error">please enter a valid time</p> : null}
         </div>
         <div className="name-container">
           <label>Name</label>
@@ -122,47 +214,56 @@ function BookingForm({}) {
             onChange={(e) =>
               handleChange(
                 "name",
-                nameRegex.test(e.target.value) ? e.target.value : ""
+                e.target.value
               )
             }
+            value={formData?.name?.value}
+            className={formData?.name?.errorMessage ? "error-field" : ""}
             type="text"
             placeholder="Enter name...."
+            onBlur={(e) => {onNameBlur()}}
           />
+          {formData?.name.errorMessage && (
+            <p className="error">{formData?.name?.errorMessage}</p>
+          )}
         </div>
         <div className="email-container">
           <label htmlFor="">Email</label>
           <input
-            // style={{ border: `${isEmailValid && "1px solid green"}` }}
-            onChange={(e) =>
-              handleChange(
-                "email",
-                emailRegex.test(e.target.value) ? e.target.value : ""
-              )
-            }
+            onChange={(e) => handleChange("email", e.target.value)}
+            value={formData?.email?.value}
+            className={formData?.email.errorMessage ? "error-field" : ""}
             type="email"
             placeholder="Enter email..."
+            onBlur={(e) => {onEmailBlur()}}
           />
+          {formData?.email.errorMessage && (
+            <p className="error">{formData?.email?.errorMessage}</p>
+          )}
         </div>
         <div className="phone-container">
           <label htmlFor="">Phone</label>
           <input
-            onChange={(e) =>
-              handleChange(
-                "phone",
-                phoneRegex.test(e.target.value) ? e.target.value : ""
-              )
-            }
+            onChange={(e) => handleChange("phone", e.target.value)}
+            value={formData?.phone?.value}
             type="number"
+            className={formData?.phone?.errorMessage ? "error-field" : ""}
             placeholder="Enter phone...."
+            onBlur={(e) => {onPhoneBlur()}}
           />
+          {formData?.phone?.errorMessage && (
+            <p className="error">{formData?.phone?.errorMessage}</p>
+          )}
         </div>
         <button
-          disabled={!isButtonEnable}
-          style={{
-            opacity: `${isButtonEnable ? "1" : "0.4"}`,
-            cursor: `${isButtonEnable ? "pointer" : "not-allowed"}`,
+          // disabled={}
+          // style={{
+          //   opacity: `${isButtonEnable ? "1" : "0.4"}`,
+          //   cursor: `${isButtonEnable ? "pointer" : "not-allowed"}`,
+          // }}
+          onClick={(e) => {
+            handleSubmit(e);
           }}
-          onClick={(e) => {handleSubmit(e)}}
           className="submitButton"
         >
           Submit
--- src/components/HorsesList.js
+++ src/components/HorsesList.js
@@ -1,23 +1,23 @@
 const horses = [
     {
       id: 1,
-      name: "Badal",
-      image: `./public/images/horse-1.jpg`,
+      name: "Apollo",
+      image: `./public/assets/horse-1.jpg`,
     },
     {
       id: 2,
-      name: "Mooti",
-      image: "./public/images/horse-2.jpg",
+      name: "Billy",
+      image: "./public/assets/horse-2.jpg",
     },
     {
       id: 3,
-      name: "Heera",
-      image: "./public/images/horse-3.jpg",
+      name: "Johnny",
+      image: "./public/assets/horse-3.jpg",
     },
     {
       id: 4,
-      name: "Shikandar",
-      image: "./public/images/horse-4.jpg",
+      name: "Blaze",
+      image: "./public/assets/horse-4.jpg",
     },
 ];
 
